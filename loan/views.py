21b5d5780ac92e8cde171a8367dcdcd3fcad48df
python class <testpython@gmail.com>
2023-07-02 13:54:24 +0530

    1files added

--- loan/views.py
+++ loan/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def loan_accounts(r):
-    return HttpResponse('<h1> Welcome to Django Project</h1>')
+    return HttpResponse('<h1> Love You Manisha</h1>')
