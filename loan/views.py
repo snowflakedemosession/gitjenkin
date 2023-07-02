d33df3498cca1cd32749658a841955caba26bef7
python class <testpython@gmail.com>
2023-07-02 18:44:37 +0530

    1files added

--- loan/views.py
+++ loan/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+##
 def loan_accounts(r):
     return HttpResponse('<h1> Hell0 From Scodeen YouTube live session</h1>')
 def loan_details(r):
