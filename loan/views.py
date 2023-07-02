1479dfa39d75124324eb06322daf8ae861ac0561
python class <testpython@gmail.com>
2023-07-02 16:00:23 +0530

    1files added

--- loan/views.py
+++ loan/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 def loan_accounts(r):
     return HttpResponse('<h1> Hell From Scodeen YouTube live session</h1>')
 def loan_details(r):
-    return HttpResponse('<h1> 1.Personal Loan</h1>')
+    return HttpResponse('<h1> 1.Personal Loan fgd</h1>')
 
 def loan_for(r):
     return HttpResponse('<h1>welocome  sadasdfd</h1>')
